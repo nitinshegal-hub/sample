98004ea8211d9787769497a12898fa93177f865e
Nitin Shegal <nitin.shegal@programming.com>
2026-03-19 10:42:42 +0530

    Duplicate console.log statements in index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-console.log("Javascript!!")
+console.log("Javascript!!")console.log("Javascript!!")
+console.log("Javascript!!")console.log("Javascript!!")console.log("Javascript!!")console.log("Javascript!!")
